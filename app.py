55064336bbf2fb4ea809c195dc4e7b8419648c34
dredpirate <moek613@gmail.com>
2018-01-15 22:48:58 -0500

    try again

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+import shabbos2_web
+
+
+
 app = Flask(__name__)
-@app.route('/')
+ 
+@app.route("/")
 def index():
-	return 'Yo, its working!'
+    #return name
+     
+    Candletime = shabbos2_web.time_stmt()
+    countdown = shabbos2_web.timetil()
+
+
+    return render_template(
+        'test.html',**locals())
+ 
 if __name__ == "__main__":
-	app.run()
+    app.run()
