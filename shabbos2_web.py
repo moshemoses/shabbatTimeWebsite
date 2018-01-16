80eb9035346ca328ddbb718fea68c0b89e4e4374
dredpirate <moek613@gmail.com>
2018-01-15 23:03:35 -0500

    try again

--- shabbos2_web.py
+++ shabbos2_web.py
@@ -24,34 +24,32 @@ def time_stmt():
 	heb_cal_address='http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year='+current_year+'&month=x&ss=on&mf=on&c=on&geo=pos&latitude=['+latit+']&longitude=['+longi+']&tzid=['+region+']&m=50&s=off'
 
 	#begin main loop
-	count = 0
-	while count<1:
-		count +=1
-		import json, requests, datetime, time
-		from subprocess import call
-		res = requests.get(heb_cal_address)
-		ready = json.loads(res.text)
-		data = ready.get('items')
-		#beginning of main while loop that should hold until candles followed by havdala
-	    
+	
+		
+	import time
+	res = requests.get(heb_cal_address)
+	ready = json.loads(res.text)
+	data = ready.get('items')
+	#beginning of main while loop that should hold until candles followed by havdala
+    
 
-		for i in range(0, len(data)):
-			if data[i].get('category') == 'candles' or data[i].get('category') == 'havdalah':
-	       			date_retrival= data[i].get('date')
-        			date_retrival2 = date_retrival.split('T')
-					time = date_retrival2[1].split('-')
-				date_plus_time = date_retrival2[0] +" "+ time[0]
-				date_obj = datetime.datetime.strptime(date_plus_time, '%Y-%m-%d %H:%M:%S')
-				event_date = date_obj.strftime('%A %B %d, %Y')
-				event_time = date_obj.strftime('%I:%M %p')
-				if date_obj >= datetime.datetime.now():
-					if data[i].get('category') == "candles":
-						event_type = "Candlelighting"
-					elif data[i].get('category') == "havdalah":
-						event_type = "Havdalah"
-					else:
-						event_type = "unclear event"
-					return( event_type +  " in " + city+ " on "+ event_date + " will be at " + event_time)
+	for i in range(0, len(data)):
+		if data[i].get('category') == 'candles' or data[i].get('category') == 'havdalah':
+			date_retrival= data[i].get('date')
+	date_retrival2 = date_retrival.split('T')
+	time = date_retrival2[1].split('-')
+	date_plus_time = date_retrival2[0] +" "+ time[0]
+	date_obj = datetime.datetime.strptime(date_plus_time, '%Y-%m-%d %H:%M:%S')
+	event_date = date_obj.strftime('%A %B %d, %Y')
+	event_time = date_obj.strftime('%I:%M %p')
+	if date_obj >= datetime.datetime.now():
+		if data[i].get('category') == "candles":
+			event_type = "Candlelighting"
+		elif data[i].get('category') == "havdalah":
+			event_type = "Havdalah"
+		else:
+			event_type = "unclear event"
+		return( event_type +  " in " + city+ " on "+ event_date + " will be at " + event_time)
 				
 
 def timetil():
@@ -93,17 +91,17 @@ def timetil():
 
 		for i in range(0, len(data)):
 			if data[i].get('category') == 'candles' or data[i].get('category') == 'havdalah':
-	       			date_retrival= data[i].get('date')
-        			date_retrival2 = date_retrival.split('T')
-				time = date_retrival2[1].split('-')
-				date_plus_time = date_retrival2[0] +" "+ time[0]
-				date_obj = datetime.datetime.strptime(date_plus_time, '%Y-%m-%d %H:%M:%S')
-				if date_obj >= datetime.datetime.now():
-					time_remain = date_obj - datetime.datetime.now()
-					time_remain1 = str(time_remain).split(" ")
-					days_remain = time_remain1[0]
-					hours_mins_remain = time_remain1[2].split(":")
-					hours_remain = hours_mins_remain[0]
-					minutes_remain = hours_mins_remain[1]
+	       			date_retrival = data[i].get('date')
+	    		date_retrival2 = date_retrival.split('T')
+			time = date_retrival2[1].split('-')
+			date_plus_time = date_retrival2[0] +" "+ time[0]
+			date_obj = datetime.datetime.strptime(date_plus_time, '%Y-%m-%d %H:%M:%S')
+			if date_obj >= datetime.datetime.now():
+				time_remain = date_obj - datetime.datetime.now()
+				time_remain1 = str(time_remain).split(" ")
+				days_remain = time_remain1[0]
+				hours_mins_remain = time_remain1[2].split(":")
+				hours_remain = hours_mins_remain[0]
+				minutes_remain = hours_mins_remain[1]
 
-					return "You have " + days_remain + " days, " + hours_remain + " hours, and " + minutes_remain +" minutes to go"
+				return "You have " + days_remain + " days, " + hours_remain + " hours, and " + minutes_remain +" minutes to go"
