1e63bdf662dff310acb94af7f67dfe4268991b03
dredpirate <moek613@gmail.com>
2018-01-15 22:39:50 -0500

    removed something from app.py

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
-
-from flask import Flask, flash, redirect, render_template, request, session, abort
-from random import randint
-import shabbos2_web
-
-
-
+from flask import Flask
 app = Flask(__name__)
- 
-@app.route("/")
+@app.route('/')
 def index():
-    #return name
-     
-    Candletime = shabbos2_web.time_stmt()
-    countdown = shabbos2_web.timetil()
-
-
-    return render_template(
-        'test.html',**locals())
- 
+	return 'Yo, its working!'
 if __name__ == "__main__":
-    app.run()
+	app.run()
